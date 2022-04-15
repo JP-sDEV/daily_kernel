9e2f9aad9d3c300babff0cd8363df585ad79395a
JP-sDEV <jon.pablo80@gmail.com>
2022-04-15 10:49:14 -0400

    init - with sample json data

---
+++ sample data/news.js
@@ -0,0 +1,266 @@
+export default {
+    "status": "ok",
+    "totalResults": 8929,
+    "articles": [
+    {
+    "source": {
+    "id": null,
+    "name": "PCMag.com"
+    },
+    "author": "Nathaniel Mott",
+    "title": "Wikimedia Community Votes to Stop Accepting Donations via Cryptocurrency",
+    "description": "The Wikimedia Foundation says its fundraising team is reviewing the request.\nWikimedia's community overwhelmingly voted in favor of a proposal for the Wikimedia Foundation, which supports Wikipedia, to stop accepting donations via cryptocurrency.Members of th…",
+    "url": "https://uk.pcmag.com/old-cryptocurrency/139838/wikimedia-community-votes-to-stop-accepting-donations-via-cryptocurrency",
+    "urlToImage": "https://sm.pcmag.com/t/pcmag_uk/news/w/wikimedia-/wikimedia-community-votes-to-stop-accepting-donations-via-cr_dskn.1200.jpg",
+    "publishedAt": "2022-04-15T14:10:23Z",
+    "content": "Wikimedia's community overwhelmingly voted in favor of a proposal for the Wikimedia Foundation, which supports Wikipedia, to stop accepting donations via cryptocurrency.\r\nMembers of the Wikimedia com… [+1784 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Freerepublic.com"
+    },
+    "author": "https://slaynews.com",
+    "title": "Elon Musk Warns: If Twitter Board ‘Takes Actions Contrary to Shareholder Interests, Liability Would Be Titanic in Scale’",
+    "description": "Elon Musk has warned the Twitter Board of Directors that, should they choose to turn down his offer to buy the company, “the liability they would thereby assume would be titanic in scale.” Musk took to Twitter to discuss the crazy day that saw him make an off…",
+    "url": "https://freerepublic.com/focus/f-chat/4055274/posts",
+    "urlToImage": null,
+    "publishedAt": "2022-04-15T14:10:19Z",
+    "content": "Skip to comments.\r\nElon Musk Warns: If Twitter Board Takes Actions Contrary to Shareholder Interests, Liability Would Be Titanic in Scalehttps://slaynews.com ^\r\n | April 14, 2022\r\n | David Hawkins\r\nP… [+6307 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Kunstler.com"
+    },
+    "author": "James Howard Kunstler",
+    "title": "You’ve Been Misinformed",
+    "description": "Half the nation doesn’t believe anything it is told by those in authority and the other half revels in its reckless abuse of authority….",
+    "url": "https://kunstler.com/clusterfuck-nation/youve-been-misinformed/",
+    "urlToImage": "http://kunstler.com/wp-content/uploads/2016/03/kunstler_thunbnail.jpg",
+    "publishedAt": "2022-04-15T14:03:06Z",
+    "content": "Clusterfuck NationFor your reading pleasure Mondays and Fridays\r\nSupport this blog by visiting Jims Patreon Page\r\nAnd thanks to all my Patrons for your support\r\nIsnt it obvious by now that pervasive … [+5055 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Sputnik International"
+    },
+    "author": "Sputnik International",
+    "title": "Tesla Denies Reports of Plant Reopening in Shanghai in Mid-May, Reports Say",
+    "description": "BEIJING (Sputnik) - US electric vehicle producer Tesla denied media reports that the production pause at the Shanghai plant due to COVID-19 lockdown will end by mid-May, local news reported on Friday.",
+    "url": "https://sputniknews.com/20220415/tesla-denies-reports-of-plant-reopening-in-shanghai-in-mid-may-reports-say-1094792144.html",
+    "urlToImage": "https://cdnn1.img.sputniknews.com/images/sharing/article/eng/1094792144.jpg?10823352191650031101",
+    "publishedAt": "2022-04-15T13:58:20Z",
+    "content": "https://sputniknews.com/20220415/tesla-denies-reports-of-plant-reopening-in-shanghai-in-mid-may-reports-say-1094792144.html\r\nTesla Denies Reports of Plant Reopening in Shanghai in Mid-May, Reports Sa… [+1902 chars]"
+    },
+    -{
+    "source": {
+    "id": null,
+    "name": "Business Today"
+    },
+    "author": "Aakanksha Chaturvedi",
+    "title": "Elon Musk: World's richest man explained in numbers",
+    "description": "Here is how much the richest person in the world is worth and here are all the assets in which his wealth is tied in.",
+    "url": "https://www.businesstoday.in/latest/story/elon-musk-worlds-richest-man-explained-in-numbers-329978-2022-04-15",
+    "urlToImage": "https://akm-img-a-in.tosshub.com/businesstoday/images/story/202204/ezgif-sixteen_nine_182.jpg",
+    "publishedAt": "2022-04-15T13:54:31Z",
+    "content": "Elon Musk, is not only a billionaire-entrepreneur, but is also the richest person alive. On the Bloomberg Billionaire Index, he ranks first with a net worth of $ 252 billion. He is also on top of the… [+2606 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Yahoo Entertainment"
+    },
+    "author": "Usman Kabir",
+    "title": "Jim Cramer Says You Should Sell These 10 Stocks",
+    "description": "In this article, we discuss the 10 stocks that Jim Cramer says you should sell. If you want to read about some more stocks that Cramer is bearish on, go...",
+    "url": "https://finance.yahoo.com/news/jim-cramer-says-sell-10-134149698.html",
+    "urlToImage": "https://s.yimg.com/uu/api/res/1.2/NCDdAKonVcpkJWfY.FcH8Q--~B/aD02MDA7dz00NTA7YXBwaWQ9eXRhY2h5b24-/https://media.zenfs.com/en/insidermonkey.com/a6c6a869a7c2c92171425bdb9b9fb0f6",
+    "publishedAt": "2022-04-15T13:41:49Z",
+    "content": "In this article, we discuss the 10 stocks that Jim Cramer says you should sell. If you want to read about some more stocks that Cramer is bearish on, go directly to Jim Cramer Says You Should Sell Th… [+10242 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "New York Post"
+    },
+    "author": "Ariel Zilber",
+    "title": "Elon Musk can’t run Tesla effectively if he buys Twitter: analysts",
+    "description": "Wall Street analysts are doubtful that Tesla CEO Elon Musk will be able to devote enough energy to running three companies if he takes over Twitter.",
+    "url": "https://nypost.com/2022/04/15/elon-musk-cant-run-tesla-effectively-if-he-buys-twitter-analysts/",
+    "urlToImage": "https://nypost.com/wp-content/uploads/sites/2/2022/04/elon-musk-tesla-twitter-comp.jpg?quality=75&strip=all&w=1024",
+    "publishedAt": "2022-04-15T13:40:35Z",
+    "content": "Wall Street analysts are doubtful that Tesla CEO Elon Musk will be able to devote enough energy to running his electric car-making company as well as SpaceX if he takes over Twitter.\r\nShares of Tesla… [+3287 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "MSPoweruser"
+    },
+    "author": "Alex Atkin",
+    "title": "Elon Musk wants to buy all of Twitter but Twitter apparently isn’t interested",
+    "description": "Elon Musk has offered to buy all of Twitter for around $43 billion, but the board has reportedly already stated that they’re not interested. In an updated SEC filing, Tesla CEO Elon Musk has made his “best and final” offer to purchase literally all of Twitter…",
+    "url": "https://mspoweruser.com/elon-musk-wants-to-buy-all-of-twitter/",
+    "urlToImage": "https://mspoweruser.com/wp-content/uploads/2022/04/Elon-Musk-Twitter-150422.jpg",
+    "publishedAt": "2022-04-15T13:38:13Z",
+    "content": "Elon Musk has offered to buy all of Twitter for around $43 billion, but the board has reportedly already stated that theyre not interested.\r\nIn an updated SEC filing, Tesla CEO Elon Musk has made his… [+1283 chars]"
+    },
+    -{
+    "source": {
+    "id": null,
+    "name": "CarScoops"
+    },
+    "author": "Sam D. Smith",
+    "title": "2023 Honda Civic Hybrid, Toyota Crown SUV In The Works, And Musk Vs Twitter: Your Morning Brief",
+    "description": "Plus, fake press release claimed Tesla bought mining company and Fiat's new 500 by Bocelli",
+    "url": "https://www.carscoops.com/2022/04/2023-honda-civic-hybrid-toyota-crown-suv-in-the-works-and-musk-vs-twitter-your-morning-brief/",
+    "urlToImage": "https://www.carscoops.com/wp-content/uploads/2022/04/Apr-15.jpg",
+    "publishedAt": "2022-04-15T13:30:13Z",
+    "content": "Good morning and welcome to our daily digest of automotive news from around the globe, starting with\r\nCalifornia Proposal Unveils Timeline To Phase Out New Gasoline Car Sales By 2035\r\nCalifornia is t… [+4186 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Dealbreaker"
+    },
+    "author": "Jon Shazar - Dealbreaker",
+    "title": "Elon Musk Makes Totally Serious, Good Faith Offer To Buy Twitter",
+    "description": "One we’re sure is every bit as well planned as the one to take Tesla private.\nThe post Elon Musk Makes Totally Serious, Good Faith Offer To Buy Twitter appeared first on Above the Law.",
+    "url": "https://dealbreaker.com/2022/04/musk-offers-to-buy-twitter",
+    "urlToImage": "https://dealbreaker.com/.image/t_share/MTYxMjc3MTk1NTY2MDAwMDky/elon_musk.jpg",
+    "publishedAt": "2022-04-15T13:28:00Z",
+    "content": "Even given the extra time he awarded himself, Elon Musk wasn’t able to buy up every Twitter share ahead of announcing his passive stake/pressure point/practical joke. But now the richest man on earth… [+1909 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "TrustedReviews"
+    },
+    "author": "Chris Smith",
+    "title": "Elon Musk buying Twitter would be great, I could finally leave",
+    "description": "OPINION: Elon Musk wants to buy Twitter to save democracy, and make it a bastion of freedom. If he takes hold, I’ll be reclaiming my freedom and ditching Twitter for good. In some perverse way, I hope Elon Musk does buy Twitter. It’ll be the excuse I need to …",
+    "url": "https://www.trustedreviews.com/opinion/elon-musk-buying-twitter-would-be-great-i-could-finally-leave-4226216",
+    "urlToImage": "https://www.trustedreviews.com/wp-content/uploads/sites/54/2022/04/FQUtQ86VQAYamc4.jpeg",
+    "publishedAt": "2022-04-15T13:27:00Z",
+    "content": "OPINION: Elon Musk wants to buy Twitter to save democracy, and make it a bastion of freedom. If he takes hold, I’ll be reclaiming my freedom and ditching Twitter for good.\r\nIn some perverse way, I ho… [+5907 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Westernjournal.com"
+    },
+    "author": "Jack Davis",
+    "title": "Elon Musk Announces He Has a 'Plan B' if Twitter Acquisition Bid Fails",
+    "description": "Elon Musk, the richest person in the world, has a Plan B in place if his quest to buy Twitter fails, but so far that remains a secret.",
+    "url": "https://www.westernjournal.com/elon-musk-announces-plan-b-twitter-acquisition-bid-fails/",
+    "urlToImage": "https://www.westernjournal.com/wp-content/uploads/2022/04/Untitled-design-2022-04-14T210345.657.jpg",
+    "publishedAt": "2022-04-15T13:26:23Z",
+    "content": "Elon Musk says he has a Plan B if his offer to buy Twitter fails, but for now, hes keeping it to himself.\r\nOn Thursday, Musk spoke at the TED2022 conference in Vancouver hours after the financial and… [+2660 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Barron's"
+    },
+    "author": "Al Root",
+    "title": "Tesla Recalls Another 594,717 Vehicles. It's the Eighth Time This Year.",
+    "description": "The company has recalled more than 2 million vehicles so far n 2022, but most of the issues involved software updates.",
+    "url": "https://www.barrons.com/articles/tesla-ev-recall-stock-51650028353",
+    "urlToImage": "https://images.barrons.com/im-517770/social",
+    "publishedAt": "2022-04-15T13:25:55Z",
+    "content": "Tesla\r\n is recalling another 590,000-plus vehicles, but the issue is another Tesla-style one. No cars need to go into the shop.The recall addresses an problem where the Boombox feature, used to broad… [+2605 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Yahoo Entertainment"
+    },
+    "author": "Hamna Asim",
+    "title": "10 Favorite Stocks of Billionaire Izzy Englander",
+    "description": "In this article, we discuss 10 favorite stocks of billionaire Izzy Englander. If you want to see more stock picks of the billionaire, go see 5 Favorite...",
+    "url": "https://finance.yahoo.com/news/10-favorite-stocks-billionaire-izzy-132352928.html",
+    "urlToImage": "https://s.yimg.com/uu/api/res/1.2/oTlK9oLVw62SLAIkfwv96A--~B/aD02MDA7dz00MjQ7YXBwaWQ9eXRhY2h5b24-/https://media.zenfs.com/en/insidermonkey.com/3e378aab45741a042bedc3618e123d5e",
+    "publishedAt": "2022-04-15T13:23:52Z",
+    "content": "In this article, we discuss 10 favorite stocks of billionaire Izzy Englander. If you want to see more stock picks of the billionaire, go see 5 Favorite Stocks of Billionaire Izzy Englander. \r\nIsrael … [+15728 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Motley Fool"
+    },
+    "author": "newsfeedback@fool.com (Rekha Khandelwal)",
+    "title": "Is Now the Time to Buy Electric Vehicle Stocks?",
+    "description": "Has the recent correction in electric vehicle stocks made them attractive?",
+    "url": "https://www.fool.com/investing/2022/04/15/is-now-the-time-to-buy-electric-vehicle-stocks/",
+    "urlToImage": "https://g.foolcdn.com/editorial/images/674365/businesswoman-charging-a-blue-electric-car.jpg",
+    "publishedAt": "2022-04-15T13:21:00Z",
+    "content": "The electric vehicle industry is in its early growth stage. It is witnessing rapid advancements in key areas such as battery costs, efficiency, range of vehicles, self-driving features, cost of vehic… [+2820 chars]"
+    },
+    -{
+    "source": {
+    "id": null,
+    "name": "Freerepublic.com"
+    },
+    "author": "Patriot Watchdog",
+    "title": "Biden Regime Launches INVESTIGATION Into Elon Musk",
+    "description": "Elon Musk is now under joint investigation by the U.S. Securities and Exchange Commission (SEC) and Department of Justice (DOJ) after making a move to buy Twitter and presumably restore free speech on the monolithic social media platform.",
+    "url": "https://freerepublic.com/focus/f-bloggers/4055259/posts",
+    "urlToImage": null,
+    "publishedAt": "2022-04-15T13:20:14Z",
+    "content": "Skip to comments.\r\nBiden Regime Launches INVESTIGATION Into Elon MuskPatriot Watchdog ^\r\n | 4/14/2022\r\n | Staff\r\nPosted on 04/15/2022 6:20:14 AM PDT by Bon of Babble\r\nElon Musk is now under joint inv… [+5190 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Freerepublic.com"
+    },
+    "author": "Patriot Watchdog",
+    "title": "Biden Regime Launches INVESTIGATION Into Elon Musk",
+    "description": "Elon Musk is now under joint investigation by the U.S. Securities and Exchange Commission (SEC) and Department of Justice (DOJ) after making a move to buy Twitter and presumably restore free speech on the monolithic social media platform.",
+    "url": "https://freerepublic.com/focus/f-news/4055259/posts",
+    "urlToImage": null,
+    "publishedAt": "2022-04-15T13:20:14Z",
+    "content": "Skip to comments.\r\nBiden Regime Launches INVESTIGATION Into Elon MuskPatriot Watchdog ^\r\n | 4/14/2022\r\n | Staff\r\nPosted on 04/15/2022 6:20:14 AM PDT by Bon of Babble\r\nElon Musk is now under joint inv… [+5273 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Yahoo Entertainment"
+    },
+    "author": "David Nadelle",
+    "title": "Taxes 2022: Don’t Forget to Use These 7 Green Tax Credits When You File",
+    "description": "With just days to go to file your 2021 tax return, you need to make sure you have exhausted all opportunities to save money and claim available credit where ...",
+    "url": "https://finance.yahoo.com/news/taxes-2022-don-t-forget-131816208.html",
+    "urlToImage": "https://s.yimg.com/ny/api/res/1.2/j9sdcu53Z0VZkkUy1KYWZw--/YXBwaWQ9aGlnaGxhbmRlcjt3PTEyMDA7aD02NzU-/https://s.yimg.com/uu/api/res/1.2/7NYA7fWHLA7Y31kr.H8yfg--~B/aD0xMDgwO3c9MTkyMDthcHBpZD15dGFjaHlvbg--/https://media.zenfs.com/en/gobankingrates_644/aa5bcbcb3ae3da50224266d5785a449b",
+    "publishedAt": "2022-04-15T13:18:16Z",
+    "content": "With just days to go to file your 2021 tax return, you need to make sure you have exhausted all opportunities to save money and claim available credit where you can. For many eco-aware homeowners and… [+4960 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "TheWrap"
+    },
+    "author": "Joe Bel Bruno",
+    "title": "Elon Musk Pulls Master Class in Trolling With Twitter Takeover Bid – But There Are Real Consequences",
+    "description": "Elon Musk just pulled off a master class in trolling with a tweet that sent Silicon Valley’s notoriously woke tech workers diving for their nearest safe space. And while it may be fun and games for the world’s richest man, there are some real-world consequenc…",
+    "url": "http://www.thewrap.com/elon-musk-twitter-hostile-takeover-bid-troll/",
+    "urlToImage": "https://www.thewrap.com/wp-content/uploads/2022/04/elon-musk-twitter-3.jpg",
+    "publishedAt": "2022-04-15T13:15:00Z",
+    "content": "Only the worlds richest human, with the reach of 81 million Twitter followers, could be the provocateur of a scheme right out of HBO’s Succession. But the Tesla CEO’s maneuver has some real consequen… [+3744 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Motley Fool"
+    },
+    "author": "newsfeedback@fool.com (Trevor Jennewine)",
+    "title": "My Top Stock Split Growth Stock to Buy Right Now",
+    "description": "This e-commerce company could deliver 10x returns in the next 10 years.",
+    "url": "https://www.fool.com/investing/2022/04/15/my-top-stock-split-growth-stock-to-buy-right-now/",
+    "urlToImage": "https://g.foolcdn.com/editorial/images/674514/growth-2.jpg",
+    "publishedAt": "2022-04-15T13:13:00Z",
+    "content": "Several popular companies have announced stock splits this year, including search giant Alphabet, retail titan Amazon, and electric car pioneer Tesla. Earlier this week Shopify( SHOP -4.18% ) joined … [+4173 chars]"
+    }
+    ]
+    }
