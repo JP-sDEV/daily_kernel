3333a5add0dc2188b89123bad47e327e62c9141d
JP-sDEV <jon.pablo80@gmail.com>
2022-04-30 12:14:14 -0400

    added sample data to components, and save/like function

--- components/NewsCard.js
+++ components/NewsCard.js
@@ -1,13 +1,26 @@
-import { Container, Card, CardMedia, CardContent, Typography, Grid } from '@mui/material';
-import NewsData from "../sample_data/news";
+import { useEffect } from "react";
+import { Card, CardMedia, CardContent, Typography, Grid, Button } from '@mui/material';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import LinkIcon from '@mui/icons-material/Link';
+import { useAppContext } from '../context/AppContext';
 
-export default function NewsCard() {
+export default function NewsCard({article, liked}) {
+    const noImg = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/300px-No_image_available.svg.png"
+    
+    const { state, dispatch }= useAppContext();
 
-    // single response object from news.org api
-    const sampleResponse = NewsData.articles[0]
-
+    const handleLike = (cardUrl) => {
+        if (state["savedArticles"].some(a => a.url === cardUrl)) {
+            dispatch({type: "remove_article", value: article?.url})
+        } else {
+            dispatch({type: "add_article", value: article})
+        }
+    }
+    
     return (
-        <div>
+        // cardId
+       <Card sx={{margin: 1}}>
+
             <Grid container spacing={1} columns={4}>
 
                 <Grid item xs={2}
@@ -17,33 +30,42 @@ export default function NewsCard() {
                     alignItems="center">
                     <CardMedia 
                         component="img"
-                        image={sampleResponse.urlToImage}/>
+                        image={article?.urlToImage || noImg}/>
                 </Grid>
 
-                <Grid item xs={3}  rowSpacing={1}>
+                <Grid item xs={3} rowSpacing={1}>
 
-                    <Grid container direction="column" item> 
+                    <Grid container direction="column"> 
 
-                        <CardContent item>
+                        <CardContent>
                             <Typography variant="h5">
-                                {sampleResponse.title}
+                                {article?.title || "No title"}
                             </Typography>
                             <Typography variant="overline">
-                                {sampleResponse.source.name} - {sampleResponse.author}
+                                {article?.source?.name || "No Source"} - {article?.author || "No Author"} 
                             </Typography>
                         </CardContent>
 
-                        <CardContent item>
+                        <CardContent>
                             <Typography variant="body2">
-                                {sampleResponse.description}
+                                {article?.description || "No description"}
                             </Typography>
                         </CardContent>
-                        
+
+                        <CardContent>
+                            <Button onClick={() => handleLike(article?.url)}>
+                                <FavoriteIcon color={liked ? "secondary" : "primary"}/>
+                            </Button>
+                            <Button>
+                                <LinkIcon color="primary"/>
+                            </Button>
+                        </CardContent>
+
                     </Grid>
 
                 </Grid>
 
             </Grid>
-        </div>
+        </Card>
     )
 }
