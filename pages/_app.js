06ba381105ca16a60452d498aa9431b7e3e24625
JP-sDEV <jon.pablo80@gmail.com>
2022-04-30 12:15:58 -0400

    added theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
+import { createTheme, ThemeProvider } from '@mui/material';
+import CustomTheme from "../styles/theme";
 import '../styles/globals.css'
+import Nav from '../components/Nav';
+
+import { AppWrapper } from '../context/AppContext';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  const theme = createTheme(CustomTheme)
+
+  return (
+    <AppWrapper>
+      <ThemeProvider theme={theme}>
+            <Nav />
+            <Component {...pageProps} />
+          </ThemeProvider>
+    </AppWrapper>
+    
+  )
 }
 
 export default MyApp
