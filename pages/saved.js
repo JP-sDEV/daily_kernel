cb387606845f0e9185231e5b94b1777f1aeac982
JP-sDEV <jon.pablo80@gmail.com>
2022-05-11 09:28:20 -0400

    mapped out articles in saved page

--- pages/saved.js
+++ pages/saved.js
@@ -1,20 +1,34 @@
-import React, { useEffect } from 'react'
+import React from 'react'
+import { Container, Grid, ListItem } from '@mui/material';
 import { useAppContext } from '../context/AppContext';
 import NewsCard from '../components/NewsCard';
 
 export default function Saved() {
 
-    const { state, dispatch }= useAppContext();
-
-
-    // useEffect(() => {
-    //     dispatch({type: "add_article", value:"test_value"})
-    // }, [])
+    const { state }= useAppContext();
 
     return(
-        <div>
-            <h1>Hello from the "Saved" page!</h1>
-        </div>
+        <Container maxWidth={"xl"}>
+            
+            <Grid
+                container
+                direction="column"
+                justifyContent="flex-end"
+                alignItems="center"
+                >
+                
+                    {state["savedArticles"].map((article, key) => {
+                    const liked = state["savedArticles"].some(a => a.url === article.url)
+                        return (
+                            <ListItem key={key}>
+                            <NewsCard key={key} article={article} liked={liked}/>
+                            </ListItem>
+                            )
+                    })}
+
+            </Grid>
+            
+        </Container>
         
     )
 }
