acece1f8cb9df00fa1b922e4d3dc02260e176db1
JP-sDEV <jon.pablo80@gmail.com>
2022-05-11 09:27:26 -0400

    removed "key" error in mapping responses

--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,35 @@
-import { Container } from '@mui/material';
-import Box from '@mui/material/Box';
+import { Container, Grid, ListItem } from '@mui/material';
 import NewsCard from '../components/NewsCard'
 import TrendingBar from '../components/TrendingBar';
-import styles from '../styles/Home.module.css'
 import { useAppContext } from '../context/AppContext';
-
 import NewsData from '../sample_data/news'
 
 export default function Home() {
   const { state }= useAppContext();
 
   const sampleResponses = NewsData.articles
-
-  const cardContainersx = {
-    display: 'flex',
-    flexDirection: 'column'
-  }
-
+  
   return (
-    <div className={styles.container}>
-      <Container maxWidth={"xl"}>
-        
-        <TrendingBar />
+    <Container maxWidth={"xl"}>
+      
+      <TrendingBar />
 
-        <Box sx={cardContainersx}>
-          {sampleResponses.map((article, key) => {
-              const liked = state["savedArticles"].some(a => a.url === article.url)
-                return (
+      <Grid
+          container
+          direction="column"
+          justifyContent="flex-end"
+          alignItems="center"
+        >
+        {sampleResponses.map((article, key) => {
+            const liked = state["savedArticles"].some(a => a.url === article.url)
+              return (
+                <ListItem key={key}>
                   <NewsCard key={key} article={article} liked={liked}/>
-                  )
-              })}
-        </Box>
-        
-      </Container>
-
-    </div>
+                </ListItem>
+                )
+            })}
+      </Grid>
+      
+    </Container>
   )
 }
