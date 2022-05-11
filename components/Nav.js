f6db6964c80c6e47ecdb8198dd4d0f25a37f0843
JP-sDEV <jon.pablo80@gmail.com>
2022-05-11 09:23:36 -0400

    added router to navbar

--- components/Nav.js
+++ components/Nav.js
@@ -1,43 +1,49 @@
 import { AppBar, Box, Toolbar, Typography, Button, Grid } from '@mui/material';
 import Image from 'next/image'
 import Link from 'next/Link';
+import {useRouter} from 'next/router';
 
 export default function Nav() {
+
+  const router = useRouter()
+
   return (
-    <div>
-      <Box>
-        <AppBar position="static" color="transparent">
-            <Toolbar>
-              <Link href="/"> 
-                <a>
-                  <Image src="/logo.png"  width="50"  height="20"/>
-                </a>
-              </Link>
-             
+    <AppBar position="static" color="transparent">
+        <Toolbar>
+            <Link href="/"> 
+              <a>
+                <Image src="/logo.png"  width="55" height="55"/>
+              </a>
+            </Link>
+            
+            <Typography sx={{ flexGrow: 1, margin:"auto" }}>the daily kernel</Typography>
 
-            {/* fills the mid part (puts buttons on each side) */}
-                <Typography sx={{ flexGrow: 1 }} />
-                  <Grid
-                  container
-                  direction="row"
-                  justifyContent="flex-end"
-                  alignItems="flex-start">
-                    
-                      <Button variant="outlined">
-                        <Link href="/">
-                          Home
-                        </Link>
-                      </Button>
+            <Grid
+            container
+            display="flex"
+            direction="row"
+            justifyContent="flex-end"
+            alignItems="flex-start">
 
-                      <Button variant="outlined">
-                        <Link href="/saved">
-                          Saved Articles
-                        </Link>
-                      </Button>
-                      </Grid>
-            </Toolbar>
-        </AppBar>
-        </Box>
-    </div>
+
+              {router.route == "/saved"
+              ?
+              <Button variant="outlined">
+                <Link href="/">
+                  Home
+                </Link>
+              </Button>
+              :
+              <Button variant="outlined">
+                <Link href="/saved">
+                  Saved Articles
+                </Link>
+              </Button>
+              }
+
+            </Grid>
+
+        </Toolbar>
+    </AppBar>
   )
 }
