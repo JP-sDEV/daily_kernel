f1fbb388a5b44a56e8f3b5297b543f3e5677ce2f
JP-sDEV <jon.pablo80@gmail.com>
2022-05-11 09:25:24 -0400

    added card buttons, and formatting methods

--- components/NewsCard.js
+++ components/NewsCard.js
@@ -1,13 +1,16 @@
-import { useEffect } from "react";
-import { Card, CardMedia, CardContent, Typography, Grid, Button } from '@mui/material';
+import { useState } from "react";
+import { Card, CardMedia, CardContent, Typography, Grid, Button, Modal, Box } from '@mui/material';
 import FavoriteIcon from '@mui/icons-material/Favorite';
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
 import LinkIcon from '@mui/icons-material/Link';
 import { useAppContext } from '../context/AppContext';
+import moment from 'moment'
 
 export default function NewsCard({article, liked}) {
     const noImg = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/300px-No_image_available.svg.png"
-    
     const { state, dispatch }= useAppContext();
+
+    const [copyModal, setCopyModal] = useState(false)
 
     const handleLike = (cardUrl) => {
         if (state["savedArticles"].some(a => a.url === cardUrl)) {
@@ -16,23 +19,48 @@ export default function NewsCard({article, liked}) {
             dispatch({type: "add_article", value: article})
         }
     }
+
+    const formatDescription = (text) => {
+        const numChars = 175
+
+        if (text.length >= numChars) {
+            return (text.slice(0, numChars)+"...")
+        }
+        
+        return text
+    }
+
+    const handleCopyModal = (url) => {
+        navigator.clipboard.writeText(url)
+        alert("Link Copied!")
+    }
     
+    const cardSX = {
+        card: {
+            width: "100%",
+            maxHeight: "30vh",
+        },
+        
+        image: {
+            width: "100%",
+        }
+    }
+
     return (
-        // cardId
-       <Card sx={{margin: 1}}>
-
+       <Card sx={cardSX.card}>
+           
             <Grid container spacing={1} columns={4}>
+                
+                    <Grid item xs={2} container>
 
-                <Grid item xs={2}
-                    container
-                    direction="row-reverse"
-                    justifyContent="center"
-                    alignItems="center">
-                    <CardMedia 
-                        component="img"
-                        image={article?.urlToImage || noImg}/>
-                </Grid>
+                        <CardMedia 
+                            component="img"
+                            image={article?.urlToImage || noImg}
+                            sx={cardSX.image}
+                            />
 
+                    </Grid>
+                
                 <Grid item xs={3} rowSpacing={1}>
 
                     <Grid container direction="column"> 
@@ -41,24 +69,29 @@ export default function NewsCard({article, liked}) {
                             <Typography variant="h5">
                                 {article?.title || "No title"}
                             </Typography>
+
                             <Typography variant="overline">
-                                {article?.source?.name || "No Source"} - {article?.author || "No Author"} 
+                                {article?.source?.name || "No Source"}{` - ${moment(article?.publishedAt).format('YYYY/MM/DD')}` || ""}
                             </Typography>
                         </CardContent>
 
                         <CardContent>
                             <Typography variant="body2">
-                                {article?.description || "No description"}
+                                {formatDescription(article?.description) || "No description"}
                             </Typography>
                         </CardContent>
 
                         <CardContent>
                             <Button onClick={() => handleLike(article?.url)}>
-                                <FavoriteIcon color={liked ? "secondary" : "primary"}/>
+                                {liked? 
+                                    <FavoriteIcon color="error"/>
+                                    : 
+                                    <FavoriteBorderOutlinedIcon color="secondary"/>}
                             </Button>
-                            <Button>
-                                <LinkIcon color="primary"/>
+                            <Button onClick={() => handleCopyModal(article?.url)}>
+                                <LinkIcon color="secondary"/>
                             </Button>
+
                         </CardContent>
 
                     </Grid>
